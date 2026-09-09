ae56b399b28ee3be3230b5762917a30bc4712e00
Louise <louise.jensen@noaignite.com>
2026-09-09 15:34:53 +0200

    retterlser2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ import { client } from "@/sanity/client";
 const POSTS_QUERY = `*[
   _type == "post"
   && defined(slug.current)
-]|order(publishedAt desc)[0...12]{_id, title, slug, publishedAt}`;
+]|order(publishedAt desc){_id, title, slug, publishedAt}`;
 
 const options = { next: { revalidate: 30 } };
 
